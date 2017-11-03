f3e8ac1607c2fe50a0fb4ba1ca31d0a09d4dcf1f
Desno365 <desno365@gmail.com>
2017-11-03 16:12:45 +0100

    Update dependencies (had to change v7 NotificationCompat to v4 NotificationCompat)

--- app/src/main/java/com/desno365/mods/DesnoUtils.java
+++ app/src/main/java/com/desno365/mods/DesnoUtils.java
@@ -39,7 +39,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.text.Spanned;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
